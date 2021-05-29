a0cec9ddb060466456cdfe8b6da40d51d6f018fd
pioka <20146647+pioka@users.noreply.github.com>
2021-05-29 18:38:54 +0900

    fcc -> butane への名称変更に追従

--- generator.sh
+++ generator.sh
@@ -2,10 +2,10 @@
 
 mkdir -p dist
 
-for fcc_file in *.fcc; do
-  ign_file="dist/$(basename $fcc_file .fcc).ign"
-  b64_file="dist/$(basename $fcc_file .fcc).b64"
+for bu_file in *.bu; do
+  ign_file="dist/$(basename $bu_file .bu).ign"
+  b64_file="dist/$(basename $bu_file .bu).b64"
 
-  docker run -i --rm quay.io/coreos/fcct:release --pretty --strict < $fcc_file > $ign_file
+  docker run -i --rm quay.io/coreos/butane:release --pretty --strict < $bu_file > $ign_file
   base64 -w0 $ign_file > $b64_file
 done
